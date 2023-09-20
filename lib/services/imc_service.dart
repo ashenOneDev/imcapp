be982a8ee9ffb1ebf2bf89605c066819f624f835
ashenOneDev <iagocav@gmail.com>
2023-09-19 22:02:53 -0300

    add classe imc_service

---
+++ lib/services/imc_service.dart
@@ -0,0 +1,27 @@
+class ImcService {
+  double calcularIMC(double altura, double peso) {
+    return peso / (altura * altura);
+  }
+
+  String classificacaoIMC(double imc) {
+    String classificacao = "";
+    if (imc < 16) {
+      classificacao = "Magreza grave";
+    } else if (imc < 17) {
+      classificacao = "Magreza moderada";
+    } else if (imc < 18.5) {
+      classificacao = "Magreza leve";
+    } else if (imc < 25) {
+      classificacao = "Saudável";
+    } else if (imc < 30) {
+      classificacao = "Sobrepeso";
+    } else if (imc < 35) {
+      classificacao = "Obesidade Grau I";
+    } else if (imc < 40) {
+      classificacao = "Obesidade Grau II (severa)";
+    } else {
+      classificacao = "Obesidade Grau III (mórbida)";
+    }
+    return classificacao;
+  }
+}
